9e0d079b412240ada2148d710411b63ad5b77a44
Nahiaa5 <nahiav2015@gmail.com>
2024-12-29 20:14:39 +0100

    unai te voy a meter una paliza

--- src/Kontroladorea/GestoreFilm.java
+++ src/Kontroladorea/GestoreFilm.java
@@ -9,12 +9,12 @@ import java.util.Observable;
 import java.util.stream.Collectors;
 import Eredua.*;
 
-public class KatalogoNagusia extends Observable {
+public class GestoreFilm extends Observable {
 	private List<Film> filmak;
 	private List<Film> jatorrizkoFilmak;
 	private static GestoreFilm nKN=null;
 	
-	private KatalogoNagusia() {
+	private GestoreFilm() {
 		filmak=new ArrayList<>();
 	}
 	
