c4b48a19583b52e87e00066fd8491b5712fd5cce
Lauracpg <laurameihua@gmail.com>
2024-12-31 02:04:51 +0100

    espero no romper nada

--- src/Kontroladorea/GestoreNagusia.java
+++ src/Kontroladorea/GestoreNagusia.java
@@ -150,14 +150,12 @@ public class GestoreNagusia extends Observable {
 		Alokairua alokairua = GestoreAlokairu.getGA().alokairuaEgin(erabNAN, film);
 		GestoreErabiltzaile.getGE().alokairuaErabiltzailearenZerrendanGehitu(alokairua);
 	}
-<<<<<<< HEAD
 	public boolean erabiltzaileakAlokatuDu(String NAN, int filmID) {
 		return GestoreFilm.getKN().erabiltzaileakAlokatuDu(NAN, filmID);
 	}
 	public String getSaioaNAN() {
 		return GestoreErabiltzaile.getGE().getSaioaNan();
-=======
-	
+	}
 	public void erabiltzaileDatuakAldatu(String pNan, String pIzena, String pAbizena, String pEmail, String pPasahitza) {
 		Erabiltzaile e = GestoreErabiltzaile.getGE().erabiltzaileaAurkitu(pNan);
 		if(pIzena.isEmpty() || pAbizena.isEmpty() || pEmail.isEmpty() || pPasahitza.isEmpty()) {
@@ -172,6 +170,5 @@ public class GestoreNagusia extends Observable {
 			setChanged();
 			notifyObservers("Sartuta");
 		}
->>>>>>> 08c25ef958af3bf5dcb73fabf76c0f442504a304
 	}
 }
